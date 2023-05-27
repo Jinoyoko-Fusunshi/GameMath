b0dacdef0f9e40ed3f6912234e5d8381a5354283
jinoyoko <retrogamer2112a@gmail.com>
2023-05-27 12:48:57 +0200

    Improved code styling.

--- Math/src/Angle.cpp
+++ Math/src/Angle.cpp
@@ -1,12 +1,14 @@
 #include <cmath>
 #include "Angle.hpp"
 
-template<FloatType N> Radian<N> Degree<N>::ToRadian() {
+template<FloatType N> 
+Radian<N> Degree<N>::ToRadian() {
     N converted_angle = angle * (M_PI / 180.0);
     return Radian(converted_angle);
 }
 
-template<FloatType N> Degree<N> Radian<N>::ToDegree() {
+template<FloatType N> 
+Degree<N> Radian<N>::ToDegree() {
     N converted_angle = angle * (180.0 / M_PI);
     return Degree(converted_angle);
 }
--- Math/src/Vector2.cpp
+++ Math/src/Vector2.cpp
@@ -1,24 +1,29 @@
 #include "Vector2.hpp"
 
-template<FloatType N> Vector2<N> Vector2<N>::operator-(Vector2<N> other) {
+template<FloatType N> 
+Vector2<N> Vector2<N>::operator-(Vector2<N> other) {
     return Vector2<N>(this->x - other.x, this->y - other.y);
 }
 
-template<FloatType N> void Vector2<N>::operator-=(Vector2<N> other) {
+template<FloatType N> 
+void Vector2<N>::operator-=(Vector2<N> other) {
     this->x -= other.x;
     this->y -= other.y;
 }
 
-template<FloatType N> Vector2<N> Vector2<N>::operator+(Vector2<N> other) {
+template<FloatType N>
+Vector2<N> Vector2<N>::operator+(Vector2<N> other) {
     return Vector2<N>(this->x + other.x, this->y + other.y);
 }
 
-template<FloatType N> void Vector2<N>::operator+=(Vector2<N> other) {
+template<FloatType N> 
+void Vector2<N>::operator+=(Vector2<N> other) {
     this->x += other.x;
     this->y += other.y;
 }
 
-template<FloatType N> bool Vector2<N>::operator==(Vector2<N> other) {
+template<FloatType N> 
+bool Vector2<N>::operator==(Vector2<N> other) {
     return this->x == other.x && this->y == other.y;
 }
 
